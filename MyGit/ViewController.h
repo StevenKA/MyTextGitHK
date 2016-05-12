d77f1131dce3d401214b262b7c583008ceb196e6
何开 <924645921@qq.com>
2016-05-12 16:17:30 +0800

    Initial Commit

---
+++ MyGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGit
+//
+//  Created by 何开 on 16/5/12.
+//  Copyright © 2016年 何开. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyGit
+//
+//  Created by 何开 on 16/5/12.
+//  Copyright © 2016年 何开. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
